7b4ad750217d3c80a8c932cecca268165f6c7582
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-08 22:09:08 +0100

    Asignar un valor retornado

---
+++ asignarValorRetornado.js
@@ -0,0 +1,16 @@
+function sumar(a, b){
+    return a + b;
+}
+
+var resultado = sumar(5, 3); // var resultado = 8;
+console.log(resultado);
+
+function crearCadenaConMeta(lenguajeDeProgramacion){
+    return "Mi meta es aprender " + lenguajeDeProgramacion;
+}
+
+var miMeta = crearCadenaConMeta("JavaScript");
+
+// var miMeta = "Mi meta es aprender JavaScript";
+
+console.log(miMeta);
