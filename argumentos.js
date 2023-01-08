ede7b5219f8e849849c663c6456bdfdd4d860003
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-08 21:53:13 +0100

    Argumentos

---
+++ argumentos.js
@@ -0,0 +1,15 @@
+function sumar(a, b){
+    var suma = a + b;
+    console.log("El resultado de " + a + " + " + b + " es: " + suma);
+}
+
+var x = 5;
+var y = 3;
+
+sumar(x, y);
+
+function concatenarTresCadenas(cadena1, cadena2, cadena3){
+    console.log(cadena1 + " " + cadena2 + " " + cadena3);
+}
+
+concatenarTresCadenas("Estoy", "aprendiendo", "a programar");
