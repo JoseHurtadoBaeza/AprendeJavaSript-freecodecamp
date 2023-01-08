b847ba1ad1757f69ba892c9aea9f3c328893a141
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-08 22:25:11 +0100

    Valores booleanos

---
+++ valoresBooleanos.js
@@ -0,0 +1,9 @@
+console.log(true);
+//console.log(True); // Error
+//console.log(TRUE); // Error
+//console.log(TrUe); // Error
+
+console.log(false);
+//console.log(False); // Error
+//console.log(FALSE); // Error
+//console.log(FaLsE); // Error
