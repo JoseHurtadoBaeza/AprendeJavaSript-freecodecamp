557a14091ea88fa2a11fed27099f16c065e10718
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-08 21:19:24 +0100

    Acceder a arrays multidimensionales

--- accederArreglosMultidimensionales.js
+++ accederArreglosMultidimensionales.js
@@ -1,1 +1,13 @@
-console.log("Hola mundo");
+var miArreglo = [[1,2,3], [4,5,6], [7,8,9]];
+
+/*
+Array:                  [[1,2,3], [4,5,6], [7,8,9]]
+Índices:                    0        1        2
+Índices internos:         0 1 2    0 1 2    0 1 2
+*/
+
+console.log(miArreglo[0]); // [1,2,3]
+console.log(miArreglo[1]); // [4,5,6]
+console.log(miArreglo[2]); // [7,8,9]
+console.log(miArreglo[1][2]); // 6 
+console.log(miArreglo[2][0]); // 7 
