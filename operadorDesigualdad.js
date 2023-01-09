b5899fb6295ffcd11f1879d1f0dc0ab6c68b9af3
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-09 01:52:09 +0100

    Operador de desigualdad

---
+++ operadorDesigualdad.js
@@ -0,0 +1,8 @@
+console.log(9 != 6); // true
+
+console.log(9 != 9); // false
+
+console.log("JavaScript" != "JavaScript"); // false
+
+console.log([1,2,3] != [1,2,3]); // true, porque estos arrays son objetos independientes en la memoria de la máquina del dispositivo
+
