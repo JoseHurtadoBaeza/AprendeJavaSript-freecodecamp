8c62d538a14a39457cb23a2fca35fa7b91b3c8cb
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-09 03:34:10 +0100

    Encadenar sentencias if... else

---
+++ encadenarIfElse.js
@@ -0,0 +1,16 @@
+function interpretarIMC(indiceDeMasaCorporal){
+    if (indiceDeMasaCorporal < 18.5){
+        console.log("Bajo peso");
+    } else if (indiceDeMasaCorporal <= 24.9){
+        console.log("Normal");
+    } else if (indiceDeMasaCorporal <= 29.9){
+        console.log("Sobrepeso")
+    } else {
+        console.log("Obeso");
+    }
+}
+
+interpretarIMC(17.8);
+interpretarIMC(22.2);
+interpretarIMC(28.5);
+interpretarIMC(32.2);
