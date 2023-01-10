825612c022aee579e1808a4e7dac3327fcb4f411
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-10 02:57:15 +0100

    Sentencias switch opción predeterminada

---
+++ sentenciasSwitchOpcionPredeterminada.js
@@ -0,0 +1,24 @@
+function seleccionarIdioma(valor){
+    var idioma;
+    switch (valor){
+        case 1:
+            idioma = "Español";
+            break;
+        case 2:
+            idioma = "Francés";
+            break;
+        case 3:
+            idioma = "Italiano";
+            break;
+        default:
+            idioma = "Inglés";
+            break;
+    }
+
+    return idioma;
+}
+
+console.log(seleccionarIdioma(1));
+console.log(seleccionarIdioma(2));
+console.log(seleccionarIdioma(3));
+console.log(seleccionarIdioma(4));
