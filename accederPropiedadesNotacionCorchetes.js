826e462a17bd6f907c09db82f08c0bbd9abc8d6f
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-10 04:43:11 +0100

    Acceder propiedades: Notación de corchetes

---
+++ accederPropiedadesNotacionCorchetes.js
@@ -0,0 +1,9 @@
+var miCuaderno = {
+    "color": "verde",
+    "categoria": 3,
+    "numero de paginas": 200,
+    "numero de hojas": 100
+};
+
+console.log(miCuaderno["color"]);
+console.log(miCuaderno["numero de hojas"]); // La ventaja de la notación de corchetes es que puedes usar espacios en blanco
