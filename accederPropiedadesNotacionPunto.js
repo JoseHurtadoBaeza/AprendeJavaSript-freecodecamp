d4c5fd4a0a12e70c65508dacc24557f540828282
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-10 04:19:47 +0100

    Acceder a propiedades: Notación de punto

---
+++ accederPropiedadesNotacionPunto.js
@@ -0,0 +1,11 @@
+var miPerro = {
+    "nombre": "Gino",
+    "edad": 5,
+    "peso": 6,
+    "raza": "Beagle"
+}
+
+console.log(miPerro.nombre);
+console.log(miPerro.edad);
+console.log(miPerro.peso);
+console.log(miPerro.raza);
