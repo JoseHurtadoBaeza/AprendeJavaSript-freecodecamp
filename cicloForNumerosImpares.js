55f773501e6eda7bc53f3ee97a98492dd5508f76
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-11 21:53:14 +0100

    Ciclo for: números impares

---
+++ cicloForNumerosImpares.js
@@ -0,0 +1,17 @@
+var miArray = [];
+
+for (var i = 1; i <= 50; i += 2) {
+    miArray.push(i);
+}
+
+console.log(miArray);
+
+// Ejercicio propuesto en el que se nos pide crear un for que agregue los números pares desde 2 hasta 26 a un array creado previamente.
+
+miArray = [];
+
+for(var i = 2; i <= 26; i += 2){
+    miArray.push(i);
+}
+
+console.log(miArray);
