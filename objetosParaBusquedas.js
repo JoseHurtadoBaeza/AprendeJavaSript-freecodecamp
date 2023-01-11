dc5a2a592a5b7e2d74b46a0bc43206649aa90d7d
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-11 04:40:38 +0100

    Objetos para búsquedas

---
+++ objetosParaBusquedas.js
@@ -0,0 +1,43 @@
+function buscarElementoQuimico(simbolo){
+    var elementoQuimico = "";
+
+    var simbolosQuimicos = {
+        "Al": "Aluminio",
+        "S": "Azufre",
+        "Cl": "Cloro",
+        "He": "Helio",
+        "B": "Boro",
+        "Li": "Litio"
+    }
+
+    /*switch(simbolo){
+        case "Al":
+            elementoQuimico = "Aluminio";
+            break;
+        case "S":
+            elementoQuimico = "Azufre";
+            break;
+        case "Cl":
+            elementoQuimico = "Cloro";
+            break;
+        case "He":
+            elementoQuimico = "Helio";
+            break;
+        case "B":
+            elementoQuimico = "Boro";
+            break;
+        case "Li":
+            elementoQuimico = "Litio";
+            break;
+    }*/
+
+    // Refactorización
+    return simbolosQuimicos[simbolo];
+}
+
+console.log(buscarElementoQuimico("Al"));
+console.log(buscarElementoQuimico("S"));
+console.log(buscarElementoQuimico("Cl"));
+console.log(buscarElementoQuimico("He"));
+console.log(buscarElementoQuimico("B"));
+console.log(buscarElementoQuimico("Li"));
