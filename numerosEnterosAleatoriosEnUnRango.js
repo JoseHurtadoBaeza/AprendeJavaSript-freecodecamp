c471751704f109abf2e2c757b3267776e7abd186
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-12 03:27:17 +0100

    Números enteros aleatorios en un rango

---
+++ numerosEnterosAleatoriosEnUnRango.js
@@ -0,0 +1,7 @@
+function rangoAleatorio(limiteInferior, limiteSuperior){
+    return Math.floor(Math.random() * (limiteSuperior - limiteInferior + 1)) + limiteInferior; // El límite superior se incluye
+}
+
+for(var i=0; i < 15; i++){
+    console.log(rangoAleatorio(3,8));
+}
