61581ca191d5a0f955c88d5b211175b72c2451a8
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-13 05:23:06 +0100

    var vs let

---
+++ varVSlet.js
@@ -0,0 +1,11 @@
+var campista = "James";
+var campista = "David"; 
+
+console.log(campista);
+
+// Con let no podríamos volver a declararla
+
+let campista2 = "James";
+campista2 = "David";
+
+console.log(campista2);
