49beac126799fee11b0083302719224642589f4e
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-13 05:06:04 +0100

    Función parseInt con una base

---
+++ funcionParseIntConUnaBase.js
@@ -0,0 +1,5 @@
+console.log(parseInt("110111", 2)); // En el segundo argumento estamos indicando la base de ese sistema, en este caso base 2.
+console.log(parseInt("110111", 2));
+console.log(parseInt("3E0A", 16)); // En base hexadecimal, en este caso sí o sí tenemos que pasar el número como cadena de caracteres
+
+
