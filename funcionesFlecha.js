b86c6128daeb25b94985b3d7a736b273c5398211
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-14 02:30:52 +0100

    Funciones flecha

---
+++ funcionesFlecha.js
@@ -0,0 +1,6 @@
+/*const fecha = function(){ // Fijémonos en que es una función anónima
+    return new Date();
+};*/
+
+// Vamos a transformarla en una función fecha
+const fecha = () => new Date();
