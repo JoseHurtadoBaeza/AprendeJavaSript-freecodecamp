0f629576b4f4c755ff14e10f3d2891b654a9c6f4
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-01-15 05:31:35 +0100

    Sintaxis de desestructuración

---
+++ sintaxisDesestructuracion.js
@@ -0,0 +1,27 @@
+const usuario = {
+    nombre: "Gino Smith",
+    edad: 34
+};
+
+//const nombre = usuario.nombre;
+//const edad = usuario.edad;
+
+const {nombre, edad} = usuario;
+
+
+
+var coordenadas = {
+    x: 4,
+    y: 6,
+    z: 12
+};
+
+//const x = coordenadas.x;
+//const y = coordenadas.y;
+//const z = coordenadas.z;
+
+const {x,y,z} = coordenadas;
+
+console.log(x);
+console.log(y);
+console.log(y);
